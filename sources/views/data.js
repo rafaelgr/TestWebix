d8c02440cd4f3590349eefc458a548117a7dfa16
Rafael Garcia <rafa@myariadna.com>
2018-08-09 12:44:42 +0200

    Capture edit events in datatable

--- sources/views/data.js
+++ sources/views/data.js
@@ -1,11 +1,33 @@
-import {JetView} from "webix-jet";
-import {data} from "models/records";
+import { JetView } from "webix-jet";
+import { data } from "models/records";
 
-export default class DataView extends JetView{
-	config(){
-		return { view:"datatable", autoConfig:true };
+var currentRowDatatableView = null;
+var currentIdDatatableView = null;
+
+export default class DataView extends JetView {
+	config() {
+		var datatableView = {
+			view: "datatable",
+			id: "data:datatable",
+			autoConfig: true,
+			editable: true,
+			on: {
+				"onAfterEditStart": function (id) {
+					currentIdDatatableView = id.row;
+					currentRowDatatableView = this.data.pull[currentIdDatatableView];
+				},
+				"onAfterEditStop": function (state, editor, ignoreUpdate) {
+					if (state.value != state.old) {
+						currentRowDatatableView = this.data.pull[currentIdDatatableView];
+						console.log("Row has changed new row :", currentRowDatatableView);
+					}
+				}
+			}
+
+		};
+		return datatableView;
 	}
-	init(view){
+	init(view) {
 		view.parse(data);
 	}
 }
